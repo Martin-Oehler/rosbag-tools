9069cf3e36e5849bfc51dbfc1edae492d14d065e
Martin Oehler <oehler@sim.tu-darmstadt.de>
2023-05-22 16:55:21 +0200

    Added parameter for compact printing

--- bag_to_text.py
+++ bag_to_text.py
@@ -6,14 +6,18 @@ def main():
     parser = argparse.ArgumentParser(description="Print a topic")
     parser.add_argument("bag_file", help="Input ROS bag.")
     parser.add_argument("topic", help="Topic name")
+    parser.add_argument('--compact', action='store_true', help="Merge entries with identical message")
 
     args = parser.parse_args()
 
     bag = rosbag.Bag(args.bag_file)
+    previous_msg = None
     for topic, msg, t in bag.read_messages(topics=[args.topic]):
+        if not args.compact or previous_msg is None or previous_msg != msg:
+            print("------------------------")
+            print(msg)
+        previous_msg = msg
         print("Stamp:", datetime.fromtimestamp(t.to_sec()).strftime('%Y-%m-%d %H:%M:%S'))
-        print(msg)
-        print("------------------------")
     bag.close()
 
 
