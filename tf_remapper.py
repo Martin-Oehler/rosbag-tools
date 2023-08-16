f704e154c1fd3fe5f0540085321b08e361a542a3
Martin Oehler <oehler@sim.tu-darmstadt.de>
2023-08-16 18:33:55 +0200

    Added separate parameters for remap and invert

--- tf_remapper.py
+++ tf_remapper.py
@@ -11,22 +11,20 @@ from src.transform_conversions import transform_msg_to_np, transform_np_to_msg
 def parse_args():
     parser = argparse.ArgumentParser(description="Change the names of tf frames or invert the transform.")
     parser.add_argument("bag_file", help="Input ROS bag.")
-    parser.add_argument("remappings", nargs="+", help="A list of tf frame remappings (old:=new) or transform inversions (parent!child)")
+    parser.add_argument("--remap", nargs="*", help="A list of tf frame remappings in the form of 'old:=new'")
+    parser.add_argument("--invert", nargs="*", help="A list of tf transforms to invert, given as parent::child")
 
     return parser.parse_args()
 
-
-def read_mapping(remap_list: List[str]):
-    mappings = dict()
-    inversions = dict()
-    for remap in remap_list:
-        parts = remap.split(":=")
+def read_dict(parameter_list: List[str], seperator):
+    d = dict()
+    for el in parameter_list:
+        parts = el.split(seperator)
         if len(parts) == 2:
-            mappings[parts[0]] = parts[1]
-        parts = remap.split("!")
-        if len(parts) == 2:
-            inversions[parts[0]] = parts[1]
-    return mappings, inversions
+            d[parts[0]] = parts[1]
+        else:
+            print("Invalid parameter", el)
+    return d
 
 def invert_transform(transform_msg):
     child = transform_msg.child_frame_id
@@ -57,7 +55,10 @@ def remap_tf(bag_in: rosbag.Bag, bag_out: rosbag.Bag, remappings, inversions):
 
 def main():
     args = parse_args()
-    remappings, inversions = read_mapping(args.remappings)
+    mappings = read_dict(args.remap, ":=")
+    inversions = read_dict(args.invert, "::")
+    print("Mappings: ", mappings)
+    print("Inversions: ", inversions)
 
     bag_file_path_parts = os.path.splitext(args.bag_file)
     if len(bag_file_path_parts) != 2:
@@ -66,7 +67,7 @@ def main():
     output_bag_file = bag_file_path_parts[0] + "_tf_remap" + bag_file_path_parts[1]
 
     with rosbag.Bag(output_bag_file, 'w') as bag_out, rosbag.Bag(args.bag_file) as bag_in:
-        remap_tf(bag_in, bag_out, remappings, inversions)
+        remap_tf(bag_in, bag_out, mappings, inversions)
 
 
 if __name__ == "__main__":
